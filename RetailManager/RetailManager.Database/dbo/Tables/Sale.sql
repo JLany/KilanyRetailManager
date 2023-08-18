e4d86b533ad2cbe17b2c771460e7c48aa287a480
Kilany <y.m.elkilany@gmail.com>
2023-08-18 15:00:16 +0300

    Add Cash Register db tables
    The tables for the basic register system

---
+++ RetailManager/RetailManager.Database/dbo/Tables/Sale.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CashierId] NVARCHAR(128) NOT NULL,
+	[SaleDate] DATETIME2(7) NOT NULL, -- Default??
+	[SubTotal] MONEY NOT NULL,
+	[Tax] MONEY NOT NULL,
+	[Total] MONEY NOT NULL,
+
+	FOREIGN KEY ([CashierId]) REFERENCES [dbo].[User]([Id])
+)
---
+++ RetailManager/RetailManager.Database/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[SaleId] INT NOT NULL,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] MONEY NOT NULL,
+	[Tax] MONEY NOT NULL DEFAULT 0,
+
+	FOREIGN KEY ([SaleId]) REFERENCES [dbo].[Sale]([Id]),
+	FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Product]([Id])
+)
