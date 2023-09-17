7dc85461fcbb7b22f8eec121c9445019d1184e6b
Kilany <y.m.elkilany@gmail.com>
2023-09-17 13:42:43 +0300

    Create data access layer

---
+++ RetailManager/RetailManager.Core/Data/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using RetailManager.Core.Data.Models;
+using RetailManager.Core.Internal.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManager.Core.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly DatabaseConnection _dbConnection;
+
+        public UserRepository()
+        {
+            // TODO: Use DI.
+            _dbConnection = new DatabaseConnection();
+        }
+
+        public UserModel GetById(string id)
+        {
+            UserModel user = _dbConnection
+                .LoadData<UserModel, object>("dbo.spUser_GetById", new { Id = id  })
+                .FirstOrDefault();
+
+            return user;
+        } 
+    }
+
+}
---
+++ RetailManager/RetailManager.Core/Internal/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RetailManager.Core.Internal.DataAccess
+{
+    internal class DatabaseConnection
+    {
+        private readonly string _connectionString = GetConnectionString("KilanyRetailManagementDB");
+
+        // TODO: Refactor this method into a config type, and register it with DI.
+        private static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public IEnumerable<TResult> LoadData<TResult, TParams>(
+            string storedProcedure,
+            TParams parameters)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var rows = connection
+                    .Query<TResult>(storedProcedure, parameters
+                    , commandType: CommandType.StoredProcedure);
+
+                return rows;
+            }
+        }
+
+        public void SaveData<TParams>(string storedProcedure, TParams parameters)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                int rowsAffected = connection
+                    .Execute(storedProcedure, parameters
+                    , commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
